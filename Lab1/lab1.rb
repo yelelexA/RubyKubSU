32b377ae4c90236747870512d2e3e93bfa07cdb1
yelelexA <axxxeleley@gmail.com>
2022-06-21 22:43:53 +0300

    1.2-u

--- Lab1/lab1.rb
+++ Lab1/lab1.rb
@@ -23,33 +23,33 @@ system(rub)
 
 
 
-def sum_chi(chi)
-    return chislo=chi.to_s.chars.inject(0){|sum,x| sum + x.to_i }
+def summ(num)
+    return chislo=num.to_s.chars.inject(0){|sum,x| sum + x.to_i }
 end
-def max_chi(chi)
-    return chislo=chi.to_s.chars.max
+def maxx(num)
+    return chislo=num.to_s.chars.max
 end
-def min_chi(chi)
-    return chislo=chi.to_s.chars.min
+def minn(num)
+    return chislo=num.to_s.chars.min
 end
-def mult_chi(chi)
-    return chislo=chi.to_s.chars.inject(1){|sum,x| sum * x.to_i }
+def multip(num)
+    return chislo=num.to_s.chars.inject(1){|sum,x| sum * x.to_i }
 end
-def prost(chi)
-return x=(1..(chi/2)+1).select{|x| chi%x==0}.size<=1
+def prost(num)
+return x=(1..(num/2)+1).select{|x| num%x==0}.size<=1
 end
-def method_1(chi)
- return (1..(chi/2)+1).select{|x| prost(x) and chi%x==0}.max
+def method_1(num)
+ return (1..(num/2)+1).select{|x| prost(x) and num%x==0}.max
 end
-def method_2(chi)
-    return chislo=chi.to_s.chars.select{|x| x!=5 }.inject(1){|sum,x| sum * x.to_i }
+def method_2(num)
+    return chislo=num.to_s.chars.select{|x| x!=5 }.inject(1){|sum,x| sum * x.to_i }
 end
-def Nod(chi1,chi2)
- return chi1 if chi1==chi2
- return Nod([chi1,chi2].min,(chi1-chi2).abs)
+def Nod(num1,num2)
+ return num1 if num1==num2
+ return Nod([num1,num2].min,(num1-num2).abs)
 end
-def method_3(chi)
- return Nod(mult_chi(chi),(1..(chi)).select{|x| (not prost(x)) and chi%x==0 and x%2!=0}.max)
+def method_3(num)
+ return Nod(multip(num),(1..(num)).select{|x| (not prost(x)) and num%x==0 and x%2!=0}.max)
  end
 
 if ARGV.size==0
